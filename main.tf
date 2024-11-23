b6772c233413a010aafd82ea7437c20ecff22c8c
nourhanmahmoud <nourhan.khalifa12@gmail.com>
2024-11-23 16:44:21 +0200

    Added cloudfront and updated ec2 sg

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ data "http" "my_ip" {
   url = "https://checkip.amazonaws.com/"
 }
 
+data "aws_ec2_managed_prefix_list" "cloudfront" {
+ name = "com.amazonaws.global.cloudfront.origin-facing"
+}
+
 resource "tls_private_key" "example" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -29,13 +33,6 @@ resource "aws_security_group" "web_server_sg" {
   name_prefix = "web-server-sg"
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -47,6 +44,56 @@ resource "aws_security_group" "web_server_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "web_server_sg_cloudfront" {
+  description = "HTTP from CloudFront"
+  security_group_id = aws_security_group.web_server_sg.id
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  prefix_list_ids = [data.aws_ec2_managed_prefix_list.cloudfront.id]
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_instance.web_server.public_dns
+    origin_id   = "my-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols = [ "SSLv3" ]
+    }
+  }
+
+  enabled             = true
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "my-origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
   }
 }
 
@@ -74,15 +121,6 @@ resource "null_resource" "generate_ansible_hosts" {
   provisioner "local-exec" {
     command = <<EOT
       terraform output -raw private_key > ./web_server.pem
-    EOT
-  }
-  provisioner "local-exec" {
-    command = <<EOT
-      cat <<EOF > ansible_extra_vars.json
-{
-  "certbot_email": "${var.certbot_email}",
-  "certbot_domain": "${aws_instance.web_server.public_dns}"
-}
     EOT
   }
 }
